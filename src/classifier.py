e0a8d06c69a92d8b9046273a6490e35fd7e642c8
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2018-06-10 00:46:00 +0300

    Train metrics plotted

--- src/classifier.py
+++ src/classifier.py
@@ -1,14 +1,33 @@
+import keras
 from keras import Sequential
 from keras.layers import Dropout, Dense, BatchNormalization
 from keras.optimizers import Adam
+
+
+class LossHistory(keras.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+
+        self.losses = []
+        self.val_losses = []
+        self.accuracies = []
+        self.val_accuracies = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accuracies.append(logs.get('acc'))
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.val_losses.append(logs.get('val_loss'))
+        self.val_accuracies.append(logs.get('val_acc'))
 
 
 class Classifier:
     def __init__(self, number_of_labels, data_size=4096):
         self.model = Sequential()
 
-        self.model.add(BatchNormalization(input_shape=(4096, )))
-        self.model.add(Dense(128, activation='relu', input_dim=4096))
+        self.model.add(BatchNormalization(input_shape=(data_size, )))
+        self.model.add(Dense(128, activation='relu', input_dim=data_size))
 
         self.model.add(BatchNormalization())
         self.model.add(Dense(64, activation='relu'))
@@ -20,10 +39,22 @@ class Classifier:
         self.model.add(BatchNormalization())
         self.model.add(Dense(number_of_labels, activation='sigmoid'))
 
-        self.model.compile(optimizer=Adam(), loss='binary_crossentropy')
+        self.model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
 
     def train(self, train_data, labels):
-        self.model.fit(train_data, labels, shuffle=True, batch_size=32, epochs=3)
+        loss_history = LossHistory()
+
+        self.model.fit(
+            x=train_data,
+            y=labels,
+            validation_split=0.1,
+            shuffle=True,
+            batch_size=64,
+            epochs=3,
+            callbacks=[loss_history]
+        )
+
+        return loss_history
 
     def test(self, test_data):
         return self.model.predict(test_data)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import numpy
+from matplotlib import pyplot
 
 from classifier import Classifier
 from data_parser import train_dataset_parser, test_dataset_parser, train_features_parser, test_features_parser
@@ -13,16 +14,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 if sys.argv[1] == 'dataset':
     print('READING DATA FROM DATASET\n...\n')
     train_data, binary_labels, bag_of_words = train_dataset_parser('../dataset/train/')
-    # test_data, image_ids = test_dataset_parser('../dataset/test/')
+    test_data, image_ids = test_dataset_parser('../dataset/test/')
 else:
     print('READING DATA FROM FEATURES CSV\n...\n')
     train_data, binary_labels, bag_of_words = train_features_parser('../dataset/features_train.csv')
-    # test_data, image_ids = test_features_parser('../dataset/features_test.csv')
-
-#
-#
-# Create a classifier
-# classifier = Classifier(number_of_labels=len(binary_labels[0]))
 
 
 #########
@@ -38,8 +33,7 @@ print(len(train_data))
 classifier = Classifier(number_of_labels=len(binary_labels[0]))
 
 # Train the model
-classifier.train(train_data, binary_labels)
-
+metrics = classifier.train(train_data, binary_labels)
 
 ########
 # TEST #
@@ -74,11 +68,27 @@ with open('../dataset/output.csv', 'w') as f:
         labels.add(max_probability)
 
         f.write('{0},{1}\n'.format(int(image_ids[i][0]), ' '.join([str(l[0]) for l in labels])))
-        print('IMAGE:', image_ids[i][0], 'LABELS:', labels)
 
-print(result)
-print(len(result))
-print(len(result[0]))
-print('MAX:', max(result[0]))
+#
+#
+# Plot the results
+pyplot.figure('Train metrics')
 
+pyplot.subplot(221)
+pyplot.plot(metrics.losses)
+pyplot.ylabel('Train Loss')
+
+pyplot.subplot(222)
+pyplot.plot(metrics.val_losses)
+pyplot.ylabel('Validation Loss')
+
+pyplot.subplot(223)
+pyplot.plot(metrics.accuracies)
+pyplot.ylabel('Train Accuracy')
+
+pyplot.subplot(224)
+pyplot.plot(metrics.val_accuracies)
+pyplot.ylabel('Validation Accuracy')
+
+pyplot.show()
 
